9437c47d314cced9769f16701b78c4d783ad59e8
Reed Allman <rdallman10@gmail.com>
2014-07-20 00:21:27 -0700

    add more tests

--- merge_operator_test.go
+++ merge_operator_test.go
@@ -37,22 +37,46 @@ func TestNewMergeOperator(t *testing.T) {
 			DestroyDb(dbName, options)
 			options.SetCreateIfMissing(true)
 			options.SetMergeOperator(merger)
-			options.SetMaxSuccessiveMerges(5)
+			//options.SetMaxSuccessiveMerges(5)
 
 			db, err := OpenDb(options, dbName)
 			So(err, ShouldBeNil)
 			So(merger.initiated, ShouldBeTrue)
 
-			Convey("When merge the value 'foo' with 'bar' then the new value should be 'foobar'", func() {
-				wo := NewDefaultWriteOptions()
-				So(db.Put(wo, []byte("foo"), []byte("foo")), ShouldBeNil)
-				So(db.Merge(wo, []byte("foo"), []byte("bar")), ShouldBeNil)
+			wo := NewDefaultWriteOptions()
+			So(db.Put(wo, []byte("foo"), []byte("foo")), ShouldBeNil)
+			So(db.Merge(wo, []byte("foo"), []byte("bar")), ShouldBeNil)
 
-				value, err := db.Get(NewDefaultReadOptions(), []byte("foo"))
-				So(err, ShouldBeNil)
-				So(value.Data(), ShouldResemble, []byte("foobar"))
-				value.Free()
-			})
+			value, err := db.Get(NewDefaultReadOptions(), []byte("foo"))
+			So(err, ShouldBeNil)
+			So(value.Data(), ShouldResemble, []byte("foobar"))
+			value.Free()
+
+			db.Close()
+			DestroyDb(dbName, options)
+
+			dbopts := NewDefaultOptions()
+			dbopts.SetCreateIfMissing(true)
+			dbopts.SetCreateMissingColumnFamilies(true)
+
+			cfopts := NewDefaultOptions()
+			cfopts.SetMergeOperator(new(testMergeOperator))
+			defcfd := ColumnFamilyDescriptor{"default", NewDefaultOptions()}
+			cf1 := ColumnFamilyDescriptor{"hi", cfopts}
+			cfds := []ColumnFamilyDescriptor{defcfd, cf1}
+			db, cfs, err := OpenDbWithColumnFamilies(dbopts, dbName, cfds)
+			So(err, ShouldBeNil)
+			So(merger.initiated, ShouldBeTrue)
+
+			wb := NewWriteBatch()
+			wb.PutCF(cfs[1], []byte("foo"), []byte("foo"))
+			wb.MergeCF(cfs[1], []byte("foo"), []byte("bar"))
+			So(db.Write(wo, wb), ShouldBeNil)
+
+			value, err = db.GetCF(NewDefaultReadOptions(), cfs[1], []byte("foo"))
+			So(err, ShouldBeNil)
+			So(value.Data(), ShouldResemble, []byte("foobar"))
+			value.Free()
 		})
 	})
 }
